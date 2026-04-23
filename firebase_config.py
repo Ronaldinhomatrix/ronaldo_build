a0a23c4e4d8c216ebc34caab77a14cb319f7195d
Ronaldo Medeiros <ronaldo.anderson@gmail.com>
2026-04-23 17:07:04 -0300

    Incluindo configuracao do firebase no APK

---
+++ firebase_config.py
@@ -0,0 +1,14 @@
+FIREBASE_CONFIG = {
+    'apiKey': 'AIzaSyCeYhEW4QhXnxa0r4dzZLrpCZ2uGfazeBM',
+    'authDomain': 'ronaldomedeirosfisiologista.firebaseapp.com',
+    'projectId': 'ronaldomedeirosfisiologista',
+    'storageBucket': 'ronaldomedeirosfisiologista.firebasestorage.app',
+    'messagingSenderId': '242992106202',
+    'appId': '1:242992106202:web:8a8934323db196842e73e3',
+}
+
+PROJECT_ID = 'ronaldomedeirosfisiologista'
+API_KEY    = FIREBASE_CONFIG['apiKey']
+
+PAINEL_URL  = 'https://ronaldo-medeiros-fisiologista.onrender.com'
+NOTIF_TOKEN = 'notif2024!'
